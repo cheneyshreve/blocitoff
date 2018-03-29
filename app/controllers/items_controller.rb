0a8a31a7f8844615a7b2ff3118541129f1d844e2
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-29 16:23:52 -0700

    still working on ajax updates

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,28 @@ class ItemsController < ApplicationController
     else
       flash.now[:alert] = "There was an error saving the item. Please try again."
     end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "To-Do is Done!"
+    else
+      flash.now[:alert] = "There was a problem deleting the to-do item."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   private
