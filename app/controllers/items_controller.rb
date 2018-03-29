0d6829611d8b45e6088014a1fa3f21806376a281
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-29 10:19:01 -0700

    updated items controller, rspec

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,19 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    
+    @item = Item.new(item_params)
+    @item.user = current_user
+
+    if @item.save
+      redirect_to user_session_path, notice: "Item was successfully added."
+    else
+      flash.now[:alert] = "There was an error saving the item. Please try again."
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name)
   end
 
 end
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
   let(:my_user){ User.create!(email:"admin_user@admin.com", password: "password", password_confirmation: "password") }
-  let(:my_item) {Item.create!(name: "first to-do item", user: my_user) }
+  let(:my_item) {Item.create!(user: my_user, name: "to-do item") }
 
   describe "POST create" do
-    before(:each) do
-       @request.env["devise.mapping"] = Devise.mappings[:user]
-       user = FactoryBot.create(:user)
-       sign_in user
+    it "assigns a new item to @item" do
+      post :create, params: {item: { name: "to-do item" } }
+      expect(assigns(:item)).to eq Item.last
     end
 
-    it "creates a new to-do item for the user" do
-      post :create, params: {id: my_item.id, items: {name: my_item.name, user: my_item.user_id } }
-      expect(assigns(:items)).to change(Item, :count).by(1)
+    it "returns http redirect" do
+      post :create, params: { item: { name: "to-do item"} }
+      expect(response).to redirect_to(user_session_path)
     end
+
   end
 
 
